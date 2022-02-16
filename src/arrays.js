0aa1ee5d1feca2112c9c9d142e9b4e5314764f0f
Paul Beard <pbeardey@gmail.com>
2022-02-16 19:09:20 +0000

    arrays pass tests upto addToArray2

--- src/arrays.js
+++ src/arrays.js
@@ -1,21 +1,36 @@
 const getNthElement = (index, array) => {
   // your code here
+  if (index < array.length){
+    return array[index];
+  }  else
+  {
+    return array[index - array.length];
+  }
 };
 
 const arrayToCSVString = array => {
   // your code here
+  const string = array.join();
+  return string;
 };
 
 const csvStringToArray = string => {
   // your code here
+  const myArray = string.split(",");
+  return myArray;
 };
 
 const addToArray = (element, array) => {
   // your code here
+  const addToArray = array.push(element);
+  //return array;
 };
 
 const addToArray2 = (element, array) => {
   // your code here
+  let myArray = [];
+  myArray = array.concat(element);
+  return myArray;
 };
 
 const removeNthElement = (index, array) => {
