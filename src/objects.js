6b32e803542cc554397223ce565bbfe45ccdf359
Paul Beard <pbeardey@gmail.com>
2022-02-17 12:44:42 +0000

    objects tests passed up to findHondas

--- src/objects.js
+++ src/objects.js
@@ -1,29 +1,55 @@
 const createPerson = (name, age) => {
   // your code here
+  const person = {
+    name: name,
+    age: age
+  };
+  return person;
 };
 
 const getName = object => {
   // your code here
+  return object.name;
 };
 
 const getProperty = (property, object) => {
   // your code here
+  return object[property];
 };
 
 const hasProperty = (property, object) => {
   // your code here
+  if (object[property]){
+    return true;
+  }
+  return false;
+  
 };
 
 const isOver65 = person => {
   // your code here
+  if (person.age>65) {
+    return true;
+  }
+  return false;
 };
 
 const getAges = people => {
   // your code here
+  let tempArray = [];
+  for (i=0; i<people.length; i+=1){
+    tempArray.push(people[i].age);
+  }
+  return tempArray;
 };
 
 const findByName = (name, people) => {
   // your code here
+  for (i=0; i<people.length; i+=1){
+    if (people[i].name === name){
+      return people[i];
+    }
+  }
 };
 
 const findHondas = cars => {
