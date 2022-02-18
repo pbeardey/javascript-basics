3d30412e23d7603e378fe68d4bc28dc2e2826c5a
Paul Beard <pbeardey@gmail.com>
2022-02-18 15:00:35 +0000

    refactored to use array methods and callbacks instead of loops

--- src/arrays.js
+++ src/arrays.js
@@ -30,101 +30,74 @@ const addToArray2 = (element, array) => {
 
 const removeNthElement = (index, array) => {
   // your code here
-  for (let i=0; i<(array.length-1-index); i+=1){
-    array[index+i] = array[index+i+1];
-  }
-  array.length = array.length-1;
+  array.splice(index,1);
 };
 
 const numbersToStrings = numbers => {
   // your code here
-  for (let i=0; i<numbers.length; i+=1){
-    numbers[i]=numbers[i].toString();
-  }
-  return numbers;
+  const tempArray = numbers.map( number => number.toString() );
+  return tempArray;
 };
 
 const uppercaseWordsInArray = strings => {
   // your code here
-  for (let i=0; i<strings.length; i+=1){
-    strings[i]=strings[i].toUpperCase();
-  }
-  return strings;
-
+  const tempArray = strings.map ( string => string.toUpperCase() );
+  return tempArray;
 };
 
 const reverseWordsInArray = strings => {
   // your code here
-  for (let i=0; i<strings.length; i+=1){
-    let tempArray=strings[i].split('');
-    tempArray.reverse();
-    strings[i]=tempArray.join('');
-  }
-  return strings;
+  const tempArray = strings.map ( (string) => {
+    string = ((string.split('')).reverse()).join('');
+    return string;
+  } );
+  return tempArray;
 };
 
 const onlyEven = numbers => {
   // your code here
-  let tempArray = [];
-  for (let i=0; i<numbers.length; i+=1){
-    if (numbers[i]%2===0) {
-      tempArray.push(numbers[i]);
-    }
-  }
+  const tempArray = numbers.filter( (number) => {if (number%2===0) {return this}} );
   return tempArray;
-
 };
 
 const removeNthElement2 = (index, array) => {
   // your code here
-  let tempArray = [];
-  for (let i=0; i<array.length; i+=1){
-    if (i!=index) {
-      tempArray.push(array[i]);
+  const tempArray = array.filter( (element, ix)=> { 
+    if (ix!=index){
+      return element;
     }
-  }
+} )
   return tempArray;
-
 };
 
 const elementsStartingWithAVowel = strings => {
   // your code here
-  let tempArray = [];
-  for (let i=0; i<strings.length; i+=1){
-    const char=strings[i].charAt(0);
+
+  const tempArray = strings.filter( (element) => {
+    const char = element.charAt(0);
     if (char === 'a' || char ==='A' || char ==='e' || char ==='E' || char ==='i' || char ==='I' || char ==='o' || char ==='O' || char ==='u' || char ==='U' ) {
-      tempArray.push(strings[i]);
+      return element;
     }
-  }
+  } )
   return tempArray;
 };
 
 const removeSpaces = string => {
   // your code here
-  const strings = string.split(' ');
-  let tempArray = [];
-  for (let i=0; i<strings.length; i+=1){
-      tempArray.push(strings[i]);
-  }
-  const text=tempArray.join('');
-  return text;
+  string = (string.split(' ')).join('');
+  return string;
 };
 
 const sumNumbers = numbers => {
   // your code here
-  let total=0;
-  for (let i=0; i<numbers.length; i+=1){
-    total += numbers[i];
-  }
+  const total = numbers.reduce((prevTotal, currentNumber)=>{ return prevTotal + currentNumber},0 );
   return total;
 };
 
 const sortByLastLetter = strings => {
   // your code here
-  let tempArray = reverseWordsInArray(strings);
-  tempArray.sort();
-  strings = reverseWordsInArray(strings);
-  return strings;
+  strings = reverseWordsInArray((reverseWordsInArray(strings)).sort());
+ return strings;
 };
 
 module.exports = {
