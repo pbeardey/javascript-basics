bf1859ff06f8efac39b47c4ea73879f0dbae47c0
Paul Beard <pbeardey@gmail.com>
2022-02-18 15:58:03 +0000

    cleaner code for readability

--- src/booleans.js
+++ src/booleans.js
@@ -27,92 +27,93 @@ function truthiness(a) {
   // your code here
   if (a) {
     return true;
-  } else {
-    return false;
   }
+  return false;
 };
 
 function isEqual(a, b) {
   // your code here
   if (a === b) {
     return (true); 
-  } else {
-    return (false);
-  }
+  } 
+  return (false);
 };
 
 function isGreaterThan(a, b) {
   // your code here
   if (a > b) {
     return true; 
-  } else {
-    return false;
-    }
+  }
+  return false;
 };
 
 function isLessThanOrEqualTo(a, b) {
   // your code here
   if (a <= b) {
     return true; 
-  } else {
-    return false;
-    }
+  }
+  return false;
 };
 
 function isOdd(a) {
   // your code here
   if (a % 2 === 1) {
     return true; 
-  } else {
-    return false;
-    }
+  }
+  return false;
 };
 
 function isEven(a) {
   // your code here
   if (a % 2 === 0) {
     return true; 
-  } else {
-    return false;
-    }
+  } 
+  return false;
 };
 
 function isSquare(a) {
   // your code here
   if (Number.isInteger(Math.sqrt(a))) {
     return true; 
-  } else {
-   return false;
   }
+  return false;
 };
 
 function startsWith(char, string) {
   // your code here
   if (string.charAt(0) === char) {
     return true; 
-  } else {
-   return false;
   }
+  return false;
 };
 
 function containsVowels(string) {
   //your code here
-  for (let i=0; i<string.length; i+=1) {
-    let char = string.charAt(i);
-      if (char === 'a' || char ==='A' || char ==='e' || char ==='E' || char ==='i' || char ==='I' || char ==='o' || char ==='O' || char ==='u' || char ==='U' ) {
-      return true;
-    } 
+  if (string.includes('a') || string.includes('A') || 
+      string.includes('e') || string.includes('E') ||
+       string.includes('i') || string.includes('I') ||
+        string.includes('o') || string.includes('O') ||
+         string.includes('u') || string.includes('U') )
+  {
+    return true;
   }
   return false;
+
+  // for (let i=0; i<string.length; i+=1) {
+  //   let char = string.charAt(i);
+  //     if (char === 'a' || char ==='A' || char ==='e' || char ==='E' || char ==='i' || char ==='I' || char ==='o' || char ==='O' || char ==='u' || char ==='U' ) {
+  //     return true;
+  //   } 
+  // }
+  // return false;
 };
 
 function isLowerCase(string) {
   // your code here
   if (string === string.toLowerCase()) {
     return true;
-  } else {
-    return false;
   }
+  return false;
 };
 
 module.exports = {
